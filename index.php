bc998a8dd9f0fd5b226eb1f812e1c709fcad4bc4
Temirlan <flashtima@gmail.com>
2023-04-16 10:43:20 +0600

    init

---
+++ index.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Main page</title>
+    <link href="stylesheets/style.css" rel="stylesheet" type="text/css">
+    <script src="https://kit.fontawesome.com/012beec9f6.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <header>
+        <a href="/" class="logo"><i class="fa-solid fa-database"></i></i></a>
+        <nav class="navbar">
+            <ul>
+                <li><a href="/">Главная</a></li>
+                <li><a href="/blog">Мой блог</a></li>
+                <li><a href="/contact">Контакты</a></li>
+            </ul>
+        </nav>
+        <a class="login-btn">Авторизоваться</a>
+    </header>
+    <main>
+        <div class="container">
+            <div class="posts"></div>
+            <div class="profile">
+                <img src="" alt="profile-avatar">
+                <div></div>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
